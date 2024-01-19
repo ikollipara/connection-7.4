ad2377871bed367216aee8a9246206ab52b16314
Ian Kollipara <ian.kollipara@cune.edu>
2024-01-19 13:18:06 -0600

    Refactor reset password form layout and add dynamic classes

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,29 +1,30 @@
 <x-layout title="ConneCTION - Reset Password">
-  <form action="{{ route('password.update') }}" method="post">
-    @csrf
-    <input type="hidden" name="token" value="{{ $token }}">
-    <x-forms.password label="Password" name="password" x-ref="password" />
-    <ul style="list-style: circle; list-style-position:inside;" class="column is-fullwidth">
-      <li ::class="/^(?=.*[0-9])/.test($refs.password) ? 'has-text-success' : 'has-text-danger'">
-        Must contain at least one digit
-      </li>
-      <li ::class="/^(?=.*[a-z])/.test($refs.password) ? 'has-text-success' : 'has-text-danger'">
-        Must contain at least one lowercase letter
-      </li>
-      <li ::class="/^(?=.*[A-Z])/.test($refs.password) ? 'has-text-success' : 'has-text-danger'">
-        Must contain at least one uppercase letter
-
-      </li>
-      <li ::class="/^(?=.*[@#$%^&-+=()])/.test($refs.password) ? 'has-text-success' : 'has-text-danger'">
-        Must contain at least one symbol (@#$%^&-+=())
-      </li>
-      <li ::class="/.{12,}/.test($refs.password) ? 'has-text-success' : 'has-text-danger'">
-        Must be at least 12 characters long
-      </li>
-    </ul>
-    <x-forms.password label="Confirm Password" name="password_confirmation" />
-    <field>
-      <button type="submit">Reset Password</button>
-    </field>
-  </form>
+  <main x-data class="container mt-3">
+    <form action="{{ route('password.update') }}" method="post">
+      @csrf
+      <input type="hidden" name="token" value="{{ $token }}">
+      <x-forms.password label="Password" name="password" x-ref="password" value="" />
+      <ul style="list-style: circle; list-style-position:inside;" class="column is-fullwidth">
+        <li x-bind:class="/^(?=.*[0-9])/.test($refs.password) ? 'has-text-success' : 'has-text-danger'">
+          Must contain at least one digit
+        </li>
+        <li x-bind:class="/^(?=.*[a-z])/.test($refs.password) ? 'has-text-success' : 'has-text-danger'">
+          Must contain at least one lowercase letter
+        </li>
+        <li x-bind:class="/^(?=.*[A-Z])/.test($refs.password) ? 'has-text-success' : 'has-text-danger'">
+          Must contain at least one uppercase letter
+        </li>
+        <li x-bind:class="/^(?=.*[@#$%^&-+=()])/.test($refs.password) ? 'has-text-success' : 'has-text-danger'">
+          Must contain at least one symbol (@#$%^&-+=())
+        </li>
+        <li x-bind:class="/.{12,}/.test($refs.password) ? 'has-text-success' : 'has-text-danger'">
+          Must be at least 12 characters long
+        </li>
+      </ul>
+      <x-forms.password label="Confirm Password" name="password_confirmation" />
+      <field>
+        <button class="button is-primary" type="submit">Reset Password</button>
+      </field>
+    </form>
+  </main>
 </x-layout>
