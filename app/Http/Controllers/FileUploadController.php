cd6ed621e70ed344b43d20055744afd4c9f5fa80
Ian Kollipara <ian.kollipara@cune.edu>
2024-03-13 12:12:34 -0500

    Add Spatie Image library for image processing

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Http;
+use Spatie\Image\Image;
 
 class FileUploadController extends Controller
 {
@@ -22,7 +24,18 @@ class FileUploadController extends Controller
         $path = false;
         if ($file = $request->file("file") ?? $request->file("image")) {
             if ($file instanceof UploadedFile) {
-                $path = $file->store("files", "public");
+                if ($path = $file->store("files", "public")) {
+                    // @phpstan-ignore-next-line
+                    if (!App::environment("local")) {
+                        Log::debug("File uploaded to " . Storage::path($path));
+                        $webp_path = Str::beforeLast($path, ".") . ".webp";
+                        Log::debug("Converting to webp: $webp_path");
+                        Image::load(Storage::path($path))
+                            ->format("webp")
+                            ->optimize()
+                            ->save($webp_path);
+                    }
+                }
             }
         }
 
@@ -33,7 +46,20 @@ class FileUploadController extends Controller
                     ->split("/\//")
                     ->last();
                 $path = "files/" . Str::random(40) . "." . $ext;
-                Storage::disk("public")->put($path, $request->body());
+                if (Storage::disk("public")->put($path, $request->body())) {
+                    if (
+                        (Str::endsWith($path, ".webp") === false or
+                            collect(["png", "jpeg", "jpg"])->contains($ext)) and
+                        !App::environment("local")
+                    ) {
+                        $webp_path = Str::beforeLast($path, ".") . ".webp";
+                    }
+                    // @phpstan-ignore-next-line
+                    Image::load($path)
+                        ->format("webp")
+                        ->optimize()
+                        ->save(Str::beforeLast($path, ".") . ".webp");
+                }
             }
         }
 
@@ -41,7 +67,7 @@ class FileUploadController extends Controller
             return response()->json([
                 "success" => 1,
                 "file" => [
-                    "url" => Storage::url($path),
+                    "url" => Storage::url($webp_path ?? $path),
                 ],
             ]);
         } else {
